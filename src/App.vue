8e4189dbd0303d35eaa5c271eaae1a154a49c134
unknown <gonzalezjunconatalia@gmail.com>
2020-02-27 15:29:22 -0500

    agregar  template

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,6 @@
 <template>
-  <v-app>
-    <Navigator/>
-    <v-content class="grey lighten-3">
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <notifications></notifications>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import Navigator from './components/Navigator'
-
-export default {
-  name: 'App',
-  components: {
-    Navigator
-  },
-  data: () => ({
-    //
-  })
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
